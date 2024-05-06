5b588ec862c85a9ed9f36e6c79a47e689d33e475
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2024-05-06 07:56:13 +0300

    weekdays Added

--- src/Clock/Clock.jsx
+++ src/Clock/Clock.jsx
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, createElement} from "react"
 
 //stylesheet
 import style from "./clock.module.css"
 
 function Clock() {
   const [time, setTime] = useState(new Date());
+
+  const daysOfWeek = ['SUN','MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const currentDayOfWeek = daysOfWeek[time.getDay()];
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -36,15 +39,7 @@ function Clock() {
 
   return (
     <div className={style.clockCantainer}>
-      <div className={style.weekDays}>
-          <span>MON</span>
-          <span>TUE</span>
-          <span>WEN</span>
-          <span>THU</span>
-          <span>FRI</span>
-          <span>SAT</span>
-          <span>SUN</span>
-      </div>
+      <div className={style.weekDays}>{daysOfWeek.map((day) => day === currentDayOfWeek ? <span id={style.currentDayOfWeek}>{day}</span> : <span>{day}</span>)}</div>
 
       <div className={style.clock}>
         <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="style=linear"><g id="clock-stand"><path id="line" d="M18 20.5L19.5 22" stroke="#0000004f" strokeWidth="2" strokeLinecap="round"/><path id="line_2" d="M6 20.5L4.5 22" stroke="#0000004f" strokeWidth="2" strokeLinecap="round"/><path id="vector" d="M21 13C21 17.968 16.968 22 12 22C7.032 22 3 17.968 3 13C3 8.032 7.032 4 12 4C16.968 4 21 8.032 21 13Z" stroke="#0000004f" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path id="vector_2" d="M15.339 15.862L12.549 14.197C12.063 13.909 11.667 13.216 11.667 12.649V8.95898" stroke="#0000004f" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/><path id="line_3" d="M18 2L21.747 5.31064" stroke="#0000004f" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/><path id="line_4" d="M6 2L2.25304 5.31064" stroke="#0000004f" strokeWidth="2" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/></g></g></svg>
